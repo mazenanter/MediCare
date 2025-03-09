23837ab2b9d611b9b78c309edbcc5c52e564bc2a
MaZen <MazenAnter>
2025-03-09 23:46:25 +0200

    - added some colors to my colors manager

--- lib/core/theming/colors_manager.dart
+++ lib/core/theming/colors_manager.dart
@@ -1,1 +1,16 @@
-class ColorsManager {}
+import 'package:flutter/widgets.dart';
+
+class ColorsManager {
+  static const Color c196EB0 = Color(0xFF196EB0);
+  static const Color cFFFFFF = Color(0xFFFFFFFF);
+  static const Color c090A0A = Color(0xFF090A0A);
+  static const Color cEAECF0 = Color(0xFFEAECF0);
+  static const Color c040415 = Color(0xFF040415);
+  static const Color c000000 = Color(0xFF000000);
+  static const Color cCDCDD0 = Color(0xFFCDCDD0);
+  static const Color cB8B8B8 = Color(0xFFB8B8B8);
+  static const Color c6C7072 = Color(0xFF6C7072);
+  static const Color cF76B1C = Color(0xFFF76B1C);
+  static const Color cFBBC05 = Color(0xFFFBBC05);
+  static const Color cEA4335 = Color(0xFFEA4335);
+}
