b4c89aef67c5952467d012bbfe2149adcd23d74a
MaZen <MazenAnter>
2025-03-09 23:41:52 +0200

    - added font styles

--- lib/core/theming/text_styles_manager.dart
+++ lib/core/theming/text_styles_manager.dart
@@ -1,1 +1,46 @@
-class TextStylesManager {}
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medicare/core/theming/font_weight_helper.dart';
+
+class TextStylesManager {
+  static TextStyle font20Meduim = TextStyle(
+    fontSize: 20.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font15Meduim = TextStyle(
+    fontSize: 15.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font16Meduim = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.medium,
+  );
+  static TextStyle font10Bold = TextStyle(
+    fontSize: 10.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font29Bold = TextStyle(
+    fontSize: 29.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font19Regular = TextStyle(
+    fontSize: 19.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font16Bold = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeightHelper.bold,
+  );
+  static TextStyle font18Regular = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font14Regular = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+  static TextStyle font12Regular = TextStyle(
+    fontSize: 12.sp,
+    fontWeight: FontWeightHelper.regular,
+  );
+}
