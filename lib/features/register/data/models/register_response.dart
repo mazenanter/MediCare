d5e6eb5973946d27dba3796b324dbd540f123d68
MaZen <MazenAnter>
2025-03-14 07:29:03 +0200

    - added register request body model - added register response model

---
+++ lib/features/register/data/models/register_response.dart
@@ -0,0 +1,19 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'register_response.g.dart';
+
+@JsonSerializable()
+class RegisterResponse {
+  final String email;
+  final String name;
+  final String uId;
+
+  RegisterResponse({
+    required this.email,
+    required this.name,
+    required this.uId,
+  });
+
+  factory RegisterResponse.fromJson(Map<String, dynamic> json) =>
+      _$RegisterResponseFromJson(json);
+}
