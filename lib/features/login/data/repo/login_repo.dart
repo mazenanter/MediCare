d33042d7c4999d000395e0e349679faf6d5e0fb1
MaZen <MazenAnter>
2025-03-17 03:32:48 +0200

    - added google signin service to login repo

--- lib/features/login/data/repo/login_repo.dart
+++ lib/features/login/data/repo/login_repo.dart
@@ -1,14 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:medicare/core/networking/firebase_error_handler.dart';
 import 'package:medicare/core/networking/firebase_result.dart';
 import 'package:medicare/features/login/data/models/login_request_body.dart';
 
 import '../../../../core/networking/firebase_auth_service.dart';
+import '../../../../core/networking/firestore_service.dart';
+import '../../../register/data/models/register_response.dart';
 
 class LoginRepo {
   final FirebaseAuthService firebaseAuthService;
-
-  LoginRepo(this.firebaseAuthService);
+  final FirestoreService firestoreService;
+  LoginRepo(this.firebaseAuthService, this.firestoreService);
 
   Future<FirebaseResult<void>> login(
       {required LoginRequestBody loginRequestBody,
@@ -19,6 +22,29 @@ class LoginRepo {
       return const FirebaseResult.success(null);
     } catch (e) {
       return FirebaseResult.error(
+          FirebaseErrorHandler.getErrorMessage(e, context));
+    }
+  }
+
+  Future<FirebaseResult<RegisterResponse>> signInWithGoogle(
+      BuildContext context) async {
+    try {
+      UserCredential? userCredential =
+          await firebaseAuthService.signInWithGoogle();
+      if (userCredential == null) {
+        return const FirebaseResult.error("Something went wrong");
+      }
+      RegisterResponse registerResponse = RegisterResponse(
+        email: userCredential.user?.email ?? '',
+        name: userCredential.user?.displayName ?? '',
+        uId: userCredential.user?.uid ?? '',
+      );
+
+      await firestoreService.addUser(registerResponse);
+      return FirebaseResult.success(registerResponse);
+    } catch (e) {
+      return FirebaseResult.error(
+          // ignore: use_build_context_synchronously
           FirebaseErrorHandler.getErrorMessage(e, context));
     }
   }
