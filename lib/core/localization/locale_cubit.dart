bf9ac5af03791b2e68af258d22c85893495a76e0
MaZen <MazenAnter>
2025-03-18 00:13:42 +0200

    - added constants class for consts variables

--- lib/core/localization/locale_cubit.dart
+++ lib/core/localization/locale_cubit.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medicare/core/helpers/constants.dart';
 import 'package:medicare/core/helpers/shared_pref_helper.dart';
 
 class LocaleCubit extends Cubit<Locale> {
@@ -11,11 +12,13 @@ class LocaleCubit extends Cubit<Locale> {
   bool get isArabic => state.languageCode == 'ar';
   void changeLanguage(Locale locale) async {
     emit(locale);
-    await SharedPrefHelper.saveData('lang', locale.languageCode);
+    await SharedPrefHelper.saveData(
+        AppConstants.lnaguageKey, locale.languageCode);
   }
 
   Future<void> _loadSavedLocale() async {
-    final savedLocale = await SharedPrefHelper.getString('lang');
+    final savedLocale =
+        await SharedPrefHelper.getString(AppConstants.lnaguageKey);
 
     if (savedLocale != null) {
       emit(Locale(savedLocale));
