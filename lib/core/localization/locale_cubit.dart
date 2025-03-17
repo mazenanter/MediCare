20a425d2f5b6b250a286864ab02f718140e1d8f6
MaZen <MazenAnter>
2025-03-18 00:03:58 +0200

    - saved lang when user chage it - get language if not null when app starts

--- lib/core/localization/locale_cubit.dart
+++ lib/core/localization/locale_cubit.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:medicare/core/helpers/shared_pref_helper.dart';
 
 class LocaleCubit extends Cubit<Locale> {
-  LocaleCubit() : super(const Locale('en'));
+  LocaleCubit() : super(const Locale('en')) {
+    // get language when app starts
+    _loadSavedLocale();
+  }
 
   bool get isArabic => state.languageCode == 'ar';
-  void toggleLanguage() {
-    emit(isArabic ? Locale('en') : const Locale('ar'));
+  void changeLanguage(Locale locale) async {
+    emit(locale);
+    await SharedPrefHelper.saveData('lang', locale.languageCode);
+  }
+
+  Future<void> _loadSavedLocale() async {
+    final savedLocale = await SharedPrefHelper.getString('lang');
+
+    if (savedLocale != null) {
+      emit(Locale(savedLocale));
+    }
   }
 }
