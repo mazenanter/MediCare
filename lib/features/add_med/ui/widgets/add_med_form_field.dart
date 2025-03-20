70b15ee0b13fe81ab06c98ff660ea755958badcf
MaZen <MazenAnter>
2025-03-21 01:41:26 +0200

    - added add med from field - added add med form

---
+++ lib/features/add_med/ui/widgets/add_med_form_field.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/theming/colors_manager.dart';
+import '../../../../core/theming/text_styles_manager.dart';
+
+class AddMedFormField extends StatelessWidget {
+  const AddMedFormField(
+      {super.key,
+      required this.hintText,
+      this.prefIcon,
+      this.contentPadding,
+      this.readOnly,
+      this.isDropDown = false,
+      this.selectedValue,
+      this.dropdownItems,
+      this.onChanged});
+  final String hintText;
+  final Widget? prefIcon;
+  final double? contentPadding;
+  final bool? readOnly;
+  final bool isDropDown;
+  final String? selectedValue;
+  final List<String>? dropdownItems;
+  final ValueChanged<String?>? onChanged;
+  @override
+  Widget build(BuildContext context) {
+    return isDropDown
+        ? DropdownButtonFormField<String>(
+            dropdownColor: ColorsManager.cFFFFFF,
+            focusColor: ColorsManager.c196EB0,
+            icon: Icon(
+              Icons.arrow_drop_down,
+              size: 24.sp,
+            ),
+            value: selectedValue,
+            hint: Text(hintText),
+            items: dropdownItems?.map((String item) {
+              return DropdownMenuItem<String>(
+                value: item,
+                child: Text(item),
+              );
+            }).toList(),
+            onChanged: onChanged,
+            decoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: ColorsManager.c196EB0,
+                  width: 1.5,
+                ),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+                borderSide: BorderSide(
+                  color: ColorsManager.c196EB0,
+                  width: 1.5,
+                ),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(horizontal: 12, vertical: 16),
+            ),
+          )
+        : TextFormField(
+            readOnly: readOnly ?? false,
+            style: TextStylesManager.font19Regular.copyWith(
+              color: ColorsManager.c196EB0,
+            ),
+            decoration: InputDecoration(
+              contentPadding: EdgeInsets.all(contentPadding ?? 15.w),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.r),
+                borderSide: BorderSide(
+                  color: ColorsManager.cBBBBBB,
+                  width: 1.5,
+                ),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8.r),
+                borderSide: BorderSide(
+                  color: ColorsManager.c196EB0,
+                  width: 2,
+                ),
+              ),
+              prefixIcon: prefIcon,
+              hintText: hintText,
+              hintStyle: TextStylesManager.font19Regular.copyWith(
+                color: ColorsManager.cBBBBBB,
+              ),
+            ),
+          );
+  }
+}
